d903b31a28c3df65c68d906b6ad6f07b6b9a05dc
hvmonteiro <hugo.monteiro@nokia.com>
2025-03-19 19:28:29 +0000

    New Portuguese banks.

--- banks/pt/index.js
+++ banks/pt/index.js
@@ -1,4 +1,10 @@
 module.exports = [
   require('./bpi'),
-  require('./millenniumbcp')
+  require('./millenniumbcp'),
+  require('./cetelem'),
+  require('./cgd'),
+  require('./millenniumbcp'),
+  require('./montepio'),
+  require('./unibanco'),
+  require('./universo')
 ];
